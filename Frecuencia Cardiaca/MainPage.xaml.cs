1e59c2372d82eed4b4345dbdb2daeb178a724326
Efa <ef058946@gmail.com>
2024-12-09 13:36:35 -0700

    first commit

---
+++ Frecuencia Cardiaca/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Frecuencia_Cardiaca.VistaModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Frecuencia_Cardiaca
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new VMpatron(Navigation);
+        }
+    }
+}
---
+++ Frecuencia Cardiaca/VistaModel/VMpatron.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Frecuencia_Cardiaca.VistaModel
+{
+    public class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        private int _latidos;
+        private int _frecuencia;
+        private string _estado;
+        #endregion
+
+        #region CONSTRUCTOR
+        public VMpatron(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+
+        #region PROPIEDADES
+        public int Latidos
+        {
+            get { return _latidos; }
+            set { SetValue(ref _latidos, value); }
+        }
+
+        public int Frecuencia
+        {
+            get { return _frecuencia; }
+            set { SetValue(ref _frecuencia, value); }
+        }
+
+        public string Estado
+        {
+            get { return _estado; }
+            set { SetValue(ref _estado, value); }
+        }
+        #endregion
+
+        #region MÉTODOS
+        public void Calcular()
+        {
+            Frecuencia = Latidos * 4;
+
+            if (Frecuencia < 60)
+                Estado = "Frecuencia cardiaca baja";
+            else if (Frecuencia > 100)
+                Estado = "Frecuencia cardiaca alta";
+            else
+                Estado = "Frecuencia cardiaca normal";
+        }
+        #endregion
+
+        #region COMANDOS
+        public ICommand CalcularCommand => new Command(Calcular);
+        #endregion
+
+    }
+}
